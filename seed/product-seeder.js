574e2737b04d4b2439bb1b6a3c0e883bfef72678
Tanmay000009 <tanmayvyas09@gmail.com>
2021-07-26 12:05:06 +0530

    Seed code bug fixed

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -1,61 +1,66 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/ecommercee');
+mongoose.connect("mongodb://localhost/ecommercee");
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
 
 var products = [
-    new Product({
-        imgPath: "assets/images/product-1-370x270.jpg",
-        name: "Lorem ipsum dolor sit amet.",
-        price: "$779.00",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit."
-    }),
-    new Product({
-        imgPath: "assets/images/product-2-370x270.jpg",
-        name: "Lorem ipsum dolor sit amet.",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
-        price: "$79.00"
-    }),
-    new Product({
-        imgPath: "assets/images/product-3-370x270.jpg",
-        name: "Lorem ipsum dolor sit amet.",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
-        price: "$1779.00"
-    }),
-    new Product({
-        imgPath: "assets/images/product-4-370x270.jpg",
-        name: "Lorem ipsum dolor sit amet.",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
-        price: "$779.00"
-    }),
-    new Product({
-        imgPath: "assets/images/product-5-370x270.jpg",
-        name: "Lorem ipsum dolor sit amet.",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
-        price: "$79.00"
-    }),
-    new Product({
-        imgPath: "assets/images/product-6-370x270.jpg",
-        name: "Lorem ipsum dolor sit amet.",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
-        price: "$1779.00"
-    })
-]
+  new Product({
+    imgpath: "assets/images/product-1-370x270.jpg",
+    name: "Lorem ipsum dolor sit amet.",
+    price: "$779.00",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
+  }),
+  new Product({
+    imgpath: "assets/images/product-2-370x270.jpg",
+    name: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
+    price: "$79.00",
+  }),
+  new Product({
+    imgpath: "assets/images/product-3-370x270.jpg",
+    name: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
+    price: "$1779.00",
+  }),
+  new Product({
+    imgpath: "assets/images/product-4-370x270.jpg",
+    name: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
+    price: "$779.00",
+  }),
+  new Product({
+    imgpath: "assets/images/product-5-370x270.jpg",
+    name: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
+    price: "$79.00",
+  }),
+  new Product({
+    imgpath: "assets/images/product-6-370x270.jpg",
+    name: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dicta voluptas quia dolor fuga odit.",
+    price: "$1779.00",
+  }),
+];
 
+seeder(products);
 
-// here saving products to database is asynchronus, hence using done.
-
-var done =0;
-for (var i=0;i< products.length;i++) {
-    products[i].save(function(err,result) {
-        done++;
-        if (done == products.length) {
-            exit();
-        }
-    });
+async function seeder(products) {
+  let seeded = await Product.create(products);
+  exit();
 }
 
 function exit() {
-    mongoose.disconnect();
+  mongoose.disconnect();
 }
