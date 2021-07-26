c56f162e727da2493e84ff4bc669dce9643f6fd5
Tanmay000009 <tanmayvyas09@gmail.com>
2021-07-26 19:12:07 +0530

    Fixed async await syntax
    Added cart html and css

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,82 +1,88 @@
-const Product = require('../models/product');
-const User = require('../models/user');
+const Product = require("../models/product");
+const User = require("../models/user");
 
-module.exports.home = function(req, res){
-    
-    return res.render('index', {
-        title: "Home",
-        user: req.user
-    });
-}
+module.exports.home = function (req, res) {
+  return res.render("index", {
+    title: "Home",
+    user: req.user,
+  });
+};
 
-module.exports.about = function(req,res) {
-    return res.render('about-us');
-}
+module.exports.about = function (req, res) {
+  return res.render("about-us");
+};
 
-module.exports.blogDetails = function(req,res) {
-    return res.render('blog-details');
-}
+module.exports.blogDetails = function (req, res) {
+  return res.render("blog-details");
+};
 
-module.exports.blog = function(req,res) {
-    return res.render('blog');
-}
+module.exports.blog = function (req, res) {
+  return res.render("blog");
+};
 
-module.exports.productDetails = async function(req,res) {
+module.exports.productDetails = async function (req, res) {
+  try {
     var id = req.params.id;
-    await Product.findOne({_id : id}, function (err,product) {
-        if (err) {
-            console.log('Err : ',err);
-            return res.redirect('back');
-        }
-        return res.render('product-details',{product : product});
-    }).exec();
-}
+    let product = await Product.findOne({ _id: id })
+      return res.render("product-details", { product: product });
+  } catch (err) {
+    console.log("Err in finding products -> productDetails ", err);
+    return res.redirect("back");
+  }
+};
 
-module.exports.checkout = function(req,res) {
-    return res.render('checkout');
-}
+module.exports.checkout = function (req, res) {
+  return res.render("checkout");
+};
 
-module.exports.contact = function(req,res) {
-    return res.render('contact');
-}
+module.exports.contact = function (req, res) {
+  return res.render("contact");
+};
 
-module.exports.products = (req,res) => {
-    // chunkSize specifies max number of elements in a row
-    Product.find( (err,docs) => {
-        if (err) {
-            res.redirect('back');
-        }
-        res.render('products',  {products: docs});
-    })
-} 
-
-module.exports.terms = function(req,res) {
-    return res.render('terms');
-}
-
-module.exports.testimonials = function(req,res) {
-    return res.render('testimonials');
-}
-
-module.exports.login = function(req,res) {
-    if (req.isAuthenticated()){
-        return res.redirect('/');
+module.exports.products = (req, res) => {
+  // chunkSize specifies max number of elements in a row
+  Product.find((err, docs) => {
+    if (err) {
+      res.redirect("back");
     }
-    return res.render('login');
-}
+    res.render("products", { products: docs });
+  });
+};
 
-module.exports.cartAdd = async function(req,res) {
-    if (req.isAuthenticated()){
+module.exports.terms = function (req, res) {
+  return res.render("terms");
+};
 
-        let user = await User.findByIdAndUpdate({_id : req.user._id},{ $push: { cart: req.params.id } },function(err, result) {
-            if (err) {
-                console.log('Error',err);
-              res.redirect('back');
-            } else {
-              res.redirect('/checkout');
-            }
-          })
+module.exports.testimonials = function (req, res) {
+  return res.render("testimonials");
+};
+
+module.exports.cart = function (req, res) {
+  return res.render("cart");
+};
+
+module.exports.login = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  return res.render("login");
+};
+
+module.exports.cartAdd = async function (req, res) {
+  try {
+    if (req.isAuthenticated()) {
+      let user = await User.findByIdAndUpdate(
+        { _id: req.user._id },
+        { $push: { cart: req.params.id } }
+      );
+      if (user) {
+        res.redirect("back");
+      }
     } else {
-        return res.render('login');
+      return res.render("login");
     }
-    }
+  } catch (err) {
+    console.log("Error in finding user -> add to cart", err);
+    return res.redirect("back");
+  }
+};
